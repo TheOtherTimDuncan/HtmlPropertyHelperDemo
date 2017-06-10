2862107d39897c9862c29717c4724046ec0fbc57
Tim <tduncan72@gmail.com>
2017-06-10 14:34:53 -0400

    Add MVC5 application from the template

---
+++ HtmlPropertyHelperDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HtmlPropertyHelperDemo.MvcApplication" Language="C#" %>
---
+++ HtmlPropertyHelperDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HtmlPropertyHelperDemo.Startup))]
+namespace HtmlPropertyHelperDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
